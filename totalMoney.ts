c3b49a5c5b7a3e7cbeaa8649234b2ca8c5c0c9df
Thang cg <duongsag.hp11@gmail.com>
2022-06-16 17:10:25 +0700

    up

---
+++ totalMoney.ts
@@ -0,0 +1,23 @@
+let matrix = [
+    [0, 1, 1, 2],
+    [0, 5, 0, 0],
+    [2, 0, 3, 3]
+];
+
+let totalMoney = (matrix: number[][]): number => {
+    let total:number = 0;
+    const col = matrix[0].length;
+    const matrixLength = matrix.length;
+    for (let j = 0; j < col; j++) {
+        for (let i = 0; i < matrixLength; i++) {
+            if  ( matrix[i-1][j] === 0) {
+                break;
+            }
+            else
+                total += matrix[i][j]
+        }
+    }
+    return total
+}
+
+console.log(totalMoney(matrix))
